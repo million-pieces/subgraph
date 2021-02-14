8fe02f51e6e7074be0e2fbf07ce1c452e88df2c4
Araton95 <tonoyandeveloper@gmail.com>
2021-02-14 23:50:03 +0300

    Init commit  🎉

---
+++ src/mapping.ts
@@ -0,0 +1,240 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Auction,
+  BigSegmentCreated,
+  NewBulkPurchase,
+  NewSinglePurchase
+} from "../generated/Auction/Auction"
+import {
+  User,
+  Segment,
+  Purchase
+} from "../generated/schema"
+
+import * as Utils from './utils'
+
+
+function returnNewPurchaseEntity(
+  tokenId: string,
+  txHash: string,
+  purchaser: string,
+  receiver: string,
+  weiAmount: BigInt,
+  timestamp: BigInt
+): Purchase {
+  let id = tokenId + "-" + txHash
+  let purchase = new Purchase(id)
+
+  purchase.initiator = purchaser
+  purchase.buyer = receiver
+  purchase.paidAmount = weiAmount
+  purchase.timestamp = timestamp
+  purchase.transaction = txHash
+
+  return purchase
+}
+
+function returnNewSegmentEntity(
+  tokenId: string,
+  receiver: string,
+): Segment {
+  let segment = new Segment(tokenId)
+
+  segment.currentOwner = receiver
+  segment.isBigSegment = false // We have another event for big segments
+  segment.purchaseHistory = new Array<string>()
+
+  return segment
+}
+
+function returnNewUserEntity(address: string): User {
+  let user = new User(address)
+
+  user.purchaseHistory = new Array<string>()
+  user.currentSegments = new Array<string>()
+
+  return user
+}
+
+export function handleNewSinglePurchase(event: NewSinglePurchase): void {
+
+  // -----------------
+  // Purchase
+  // -----------------
+
+  let purchase = returnNewPurchaseEntity(
+    event.params.tokenId.toString(),
+    event.transaction.hash.toHex(),
+    event.params.purchaser.toHex(),
+    event.params.receiver.toHex(),
+    event.params.weiAmount,
+    event.block.timestamp
+  )
+
+  purchase.save()
+
+  // -----------------
+  // Segment
+  // -----------------
+
+  let segment = returnNewSegmentEntity(
+    event.params.tokenId.toString(),
+    event.params.receiver.toHex()
+  )
+
+  let segmentPurchaseHistory = segment.purchaseHistory
+  segmentPurchaseHistory.push(purchase.id)
+  segment.purchaseHistory = segmentPurchaseHistory
+  segment.save()
+
+  // -----------------
+  // User
+  // -----------------
+
+  let userAddress = event.params.receiver.toHex()
+  let user = User.load(userAddress)
+  if (user == null) {
+    user = returnNewUserEntity(userAddress)
+  }
+
+  let userPurchaseHistory = user.purchaseHistory,
+      userCurrentSegments = user.currentSegments
+
+  userPurchaseHistory.push(purchase.id)
+  userCurrentSegments.push(segment.id)
+  user.purchaseHistory = userPurchaseHistory
+  user.currentSegments = userCurrentSegments
+
+  user.save()
+}
+
+export function handleBigSegmentCreated(event: BigSegmentCreated): void {
+
+  let receivers = event.params.receivers
+  let tokenIds = event.params.tokenIds
+  let amountsPaid = event.params.amountsPaid
+
+  let transactionHash = event.transaction.hash.toHex()
+  let timestamp = event.block.timestamp
+  let segmentsCount = event.params.tokenIds.length
+
+  for (let i = 0; i < segmentsCount; i++) {
+    let receiver = receivers[i].toHex()
+    let tokenId = tokenIds.toString()
+    let amountPaid = amountsPaid[i]
+
+    // -----------------
+    // Purchase
+    // -----------------
+
+    let purchase = returnNewPurchaseEntity(
+      tokenId,
+      transactionHash,
+      Utils.ZERO_ADDR,
+      receiver,
+      amountPaid,
+      timestamp
+    )
+
+    purchase.save()
+
+    // -----------------
+    // Segment
+    // -----------------
+
+    let segment = returnNewSegmentEntity(
+      tokenId,
+      receiver
+    )
+
+    let segmentPurchaseHistory = segment.purchaseHistory
+    segmentPurchaseHistory.push(purchase.id)
+    segment.purchaseHistory = segmentPurchaseHistory
+    segment.isBigSegment = true // As this event happens for big segments only
+    segment.save()
+
+    // -----------------
+    // User
+    // -----------------
+
+    let user = User.load(receiver)
+    if (user == null) {
+      user = returnNewUserEntity(receiver)
+    }
+
+    let userPurchaseHistory = user.purchaseHistory,
+        userCurrentSegments = user.currentSegments
+
+    userPurchaseHistory.push(purchase.id)
+    userCurrentSegments.push(segment.id)
+    user.purchaseHistory = userPurchaseHistory
+    user.currentSegments = userCurrentSegments
+
+    user.save()
+  }
+}
+
+export function handleNewBulkPurchase(event: NewBulkPurchase): void {
+  let tokenIds = event.params.tokenIds
+  let receivers = event.params.receivers
+
+  let transactionHash = event.transaction.hash.toHex()
+  let timestamp = event.block.timestamp
+  let purchaser = event.params.purchaser.toHex()
+  let segmentsCount = event.params.tokenIds.length
+  let amountPerEachNft = event.params.ethSent.div(BigInt.fromI32(segmentsCount))
+
+  for (let i = 0; i < segmentsCount; i++) {
+    let receiver = receivers[i].toHex()
+    let tokenId = tokenIds[i].toString()
+
+    // -----------------
+    // Purchase
+    // -----------------
+
+    let purchase = returnNewPurchaseEntity(
+      tokenId,
+      transactionHash,
+      purchaser,
+      receiver,
+      amountPerEachNft,
+      timestamp
+    )
+
+    purchase.save()
+
+    // -----------------
+    // Segment
+    // -----------------
+
+    let segment = returnNewSegmentEntity(
+      tokenId,
+      receiver
+    )
+
+    let segmentPurchaseHistory = segment.purchaseHistory
+    segmentPurchaseHistory.push(purchase.id)
+    segment.purchaseHistory = segmentPurchaseHistory
+    segment.isBigSegment = true // As this event happens for big segments only
+    segment.save()
+
+    // -----------------
+    // User
+    // -----------------
+
+    let user = User.load(receiver)
+    if (user == null) {
+      user = returnNewUserEntity(receiver)
+    }
+
+    let userPurchaseHistory = user.purchaseHistory,
+        userCurrentSegments = user.currentSegments
+
+    userPurchaseHistory.push(purchase.id)
+    userCurrentSegments.push(segment.id)
+    user.purchaseHistory = userPurchaseHistory
+    user.currentSegments = userCurrentSegments
+
+    user.save()
+  }
+}
---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+import { BigInt, BigDecimal } from '@graphprotocol/graph-ts'
+
+export let ZERO_INT = BigInt.fromI32(0)
+export let ZERO_DEC = BigDecimal.fromString('0')
+export let EMPTY_STRING_ARRAY = new Array<string>()
+export let ONE_INT = BigInt.fromI32(1)
+export let ONE_DEC = BigDecimal.fromString('1')
+export let PRECISION = new BigDecimal(tenPow(18))
+export let ETH_ADDR = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+export let ZERO_ADDR = '0x0000000000000000000000000000000000000000'
+
+export function tenPow(exponent: number): BigInt {
+  let result = BigInt.fromI32(1)
+
+  for (let i = 0; i < exponent; i++) {
+    result = result.times(BigInt.fromI32(10))
+  }
+
+  return result
+}
+
+export function getArrItem<T>(arr: Array<T>, idx: i32): T {
+  let a = new Array<T>()
+  a = a.concat(arr)
+
+  return a[idx]
+}
+
+export function normalize(i: BigInt, decimals: number = 18): BigDecimal {
+  return i.toBigDecimal().div(tenPow(decimals).toBigDecimal())
+}
